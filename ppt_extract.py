b2d8491c2ce956ea149a4a397c3a56fce062f5d2
JLoeve <jianglufull@foxmail.com>
2021-10-16 14:34:38 +0800

    fix output

--- ppt_extract.py
+++ ppt_extract.py
@@ -2,15 +2,19 @@ import cv2
 import numpy as np
 import os
 import sys
+from pathlib import Path
+import shutil
+
 
 def average_hash(img):
     ihash = img > np.mean(img)
     return ihash
 
+
 def calc_diff(prev, curr, w=50, h=50):
     try:
-        prev = cv2.resize(prev, (w,h))
-        curr = cv2.resize(curr, (w,h))
+        prev = cv2.resize(prev, (w, h))
+        curr = cv2.resize(curr, (w, h))
         hash_prev = prev > np.mean(prev)
         hash_curr = curr > np.mean(curr)
         diff = hash_prev & hash_curr
@@ -18,32 +22,48 @@ def calc_diff(prev, curr, w=50, h=50):
     except:
         return 0
 
+
 def proc(fname):
-    os.mkdir(fname.split(".")[0])
-    
+    out_dir = Path(Path(fname).stem)
+    if out_dir.exists():
+        print("clear output folder")
+        shutil.rmtree(out_dir)
+    out_dir.mkdir()
+
     diff = []
     frames = []
     cap = cv2.VideoCapture(fname)
-    
+
     f = 0
     prev = None
     prev_diff = 1000
     prev_frame = -1000
-    
+
+    os.chdir(out_dir)
+
     while cap.isOpened():
         f += 1
         ret, frame = cap.read()
+        if frame is None:
+            break
+
         curr = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         curr_diff = calc_diff(prev, curr)
         diff.append(curr_diff)
+
         if abs(curr_diff - prev_diff) > 50 and f - prev_frame > 10:
             frames.append(frame)
-            ff = fname.split(".")[0]
-            cv2.imwrite("%s/%s_%d.jpg"%(ff, ff, len(frames)), frame)
+            out_path = f"{f}-{len(frame)}.png"
+            print(out_path)
+            cv2.imwrite(out_path, frame)
             prev_frame = f
+
         prev = curr
         prev_diff = curr_diff
-    print("Task: %s finished"%(fname.split(".")[0]))
+
+    print(f"Task: {Path(fname).stem} finished")
+
     return frames
+
 
 proc(sys.argv[1])
